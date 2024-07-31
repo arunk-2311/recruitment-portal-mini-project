093d42ca87c777bc46257d3cc76c76ead5057723
arunkumar <arunkumar.m@mindgate>
2024-07-31 14:08:17 +0530

    added forward to hr method

--- src/main/java/com/mindgate/recruitment/controller/PMController.java
+++ src/main/java/com/mindgate/recruitment/controller/PMController.java
@@ -1,20 +1,16 @@
 package com.mindgate.recruitment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.mindgate.recruitment.beans.JobRequest;
 import com.mindgate.recruitment.exceptions.JobFillOverflowException;
-import com.mindgate.recruitment.exceptions.JobRequestNotFoundException;
-import com.mindgate.recruitment.service.EmployeeService;
+import com.mindgate.recruitment.exceptions.JobRequestInvalidLevelException;
 import com.mindgate.recruitment.service.JobRequestService;
 
 @CrossOrigin(origins = "*")
@@ -26,14 +22,27 @@ public class PMController {
 	private JobRequestService jobRequestService;
 
 	// creates a job request
-	@PutMapping(path = "/fillfromworkbench/{id}/{count}")
-	public ResponseEntity<Object> fillFromWorkBench(@PathVariable("id") int id, @PathVariable("count") int fillCount) {		
+	@PutMapping(path = "/fillFromWorkBench/{id}/{count}")
+	public ResponseEntity<Object> fillFromWorkBench(@PathVariable("id") int requestId,
+			@PathVariable("count") int fillCount) {
 		try {
-			JobRequest j = jobRequestService.UpdateJobRequestFilled(id, fillCount);
+			JobRequest j = jobRequestService.updateJobRequestFilled(requestId, fillCount);
 			return ResponseEntity.status(201).body(j);
 		} catch (JobFillOverflowException e) {
 			return ResponseEntity.status(404).body(e.getMessage());
 		}
 	}
-	
+
+	@PutMapping(path = "/forwardToHR/{id}")
+	public ResponseEntity<Object> forwardToHR(@PathVariable("id") int requestId) {
+		try {
+			JobRequest j = jobRequestService.updateJobRequestLevel(requestId, 1);
+			return ResponseEntity.status(201).body(j);
+
+		} catch (JobRequestInvalidLevelException e) {
+			// TODO Auto-generated catch block
+			return ResponseEntity.status(404).body(e.getMessage());
+		}
+	}
+
 }
