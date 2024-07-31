c15a1580bf116f30fd7511543bf861fe595aef26
MGSCHN-USER462 <MGSCHN-USER462@MGSCHN-LAP-462>
2024-07-31 14:08:36 +0530

    added jobRequestInvalidException

---
+++ src/main/java/com/mindgate/recruitment/exceptions/JobRequestInvalidLevelException.java
@@ -0,0 +1,15 @@
+package com.mindgate.recruitment.exceptions;
+
+public class JobRequestInvalidLevelException extends Exception{
+
+	public JobRequestInvalidLevelException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public JobRequestInvalidLevelException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
