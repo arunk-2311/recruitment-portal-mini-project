57f7ec1ea168378a2f80a088f82a272f8e3499bf
arunkumar <arunkumar.m@mindgate>
2024-07-31 12:42:22 +0530

    pm service fill update

---
+++ src/main/java/com/mindgate/recruitment/service/PMServiceImplV1.java
@@ -0,0 +1,25 @@
+package com.mindgate.recruitment.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mindgate.recruitment.beans.JobRequest;
+import com.mindgate.recruitment.exceptions.JobFillOverflowException;
+
+public class PMServiceImplV1 implements PMService{
+	
+	@Autowired
+	private JobRequestService jobRequestService;
+	
+	@Override
+	public JobRequest fillJobRequest(int requestId, int num){
+		
+		try {
+			return jobRequestService.UpdateJobRequestFilled(requestId, num);
+		} catch (JobFillOverflowException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		
+		return null;
+	}
+}
