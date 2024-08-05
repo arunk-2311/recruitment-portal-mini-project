66e9d42678aa779fb09209efb9e29c93ef710688
arunkumar <arunkumar.m@mindgate>
2024-08-05 11:31:59 +0530

    forwarded emp id to login result object

--- src/main/java/com/mindgate/recruitment/beans/LoginReturnResult.java
+++ src/main/java/com/mindgate/recruitment/beans/LoginReturnResult.java
@@ -3,11 +3,16 @@ package com.mindgate.recruitment.beans;
 public class LoginReturnResult {
 	public boolean result = true;
 	public String role = "";
+	public int empId;
 	
-	public LoginReturnResult(boolean result, String role) {
+	public LoginReturnResult(boolean result, String role,int empId) {
 		super();
 		this.result = result;
 		this.role = role;
+		this.empId=empId;
+	}
+	public int getEmpId() {
+		return empId;
 	}
 	public boolean isResult() {
 		return result;
